165d20ecfc53132c88c2772b465ccef0cc92dda7
Alex Yuen <mralexyuen@gmail.com>
2018-06-04 20:55:30 -0700

    Rewritten to use LocalDateTime class

--- ATCalendar.java
+++ ATCalendar.java
@@ -1,46 +1,43 @@
 import java.util.Arrays;
-import java.util.GregorianCalendar;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-public class ATCalendar {
+public class AutomatedCalendar {
 
     enum Tournament {
         
-        Balthazar(new GregorianCalendar(2018, 4, 5, 20, 15)),
-        Grenth(new GregorianCalendar(2018, 4, 5, 3, 15)),
-        Lyssa(new GregorianCalendar(2018, 4, 5, 14, 15)),
-        Melandru(new GregorianCalendar(2018, 4, 5, 9, 15));
+        Balthazar(LocalDateTime.of(2018, 5, 5, 20, 15)),
+        Grenth(LocalDateTime.of(2018, 5, 5, 3, 15)),
+        Lyssa(LocalDateTime.of(2018, 5, 5, 14, 15)),
+        Melandru(LocalDateTime.of(2018, 5, 5, 9, 15));
 
-        private final GregorianCalendar startTime;
+        private LocalDateTime startTime;
 
-        Tournament(GregorianCalendar startTime) {
+        Tournament(LocalDateTime startTime) {
             this.startTime = startTime;
         }
     }
 
     public void printSchedule() {
-        GregorianCalendar now = new GregorianCalendar();
+        LocalDateTime now = LocalDateTime.now();
 
         for (Tournament t : Tournament.values()) {
-            while (t.startTime.before(now)) {
-                t.startTime.add(GregorianCalendar.HOUR, 23);
+            while (t.startTime.isBefore(now)) {
+                t.startTime = t.startTime.plusHours(23);
             }
         }
 
         Tournament[] next = Tournament.values();
         Arrays.sort(next, (Tournament t1, Tournament t2) -> t1.startTime.compareTo(t2.startTime));
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("h:mma E");
         for (Tournament t : next) {
-            System.out.println(t + ": " + until(now, t.startTime) + " - " + dateFormat.format(t.startTime.getTime()));
+            System.out.println(t + ": " + until(now, t.startTime) + " - " + t.startTime.format(DateTimeFormatter.ofPattern("h:mm a EEEE")));
         }
     }
 
-    private String until(GregorianCalendar from, GregorianCalendar to) {
-        long fromMillis = from.getTimeInMillis();
-        long toMillis = to.getTimeInMillis();
-        
-        long difference = toMillis - fromMillis;
+    private String until(LocalDateTime from, LocalDateTime to) {
+        long difference = ChronoUnit.MILLIS.between(from, to);
 
         //long diffSeconds = difference / 1000 % 60;
 		long diffMinutes = difference / (60 * 1000) % 60;
