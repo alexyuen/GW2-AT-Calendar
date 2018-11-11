5ec0cc82d9de097dd32c7c7a28285153f8bb1ec9
Alex Yuen <mralexyuen@gmail.com>
2018-11-11 11:10:50 -0800

    Fixed bugs with timezone conversion and DST. Base times changed to UTC.

--- ATCalendar.java
+++ ATCalendar.java
@@ -1,6 +1,6 @@
+import java.time.temporal.Temporal;
 import java.util.Arrays;
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
@@ -10,27 +10,28 @@ import java.time.ZoneId;
 public class ATCalendar {
 
     enum Tournament {
-        
-        Balthazar(fromPST(2018, 5, 5, 20, 15)),
-        Grenth(fromPST(2018, 5, 5, 3, 15)),
-        Lyssa(fromPST(2018, 5, 5, 14, 15)),
-        Melandru(fromPST(2018, 5, 5, 9, 15));
 
-        private LocalDateTime startTime;
+        Balthazar(fromUTC(2018, 5, 6, 3, 15)),
+        Grenth(fromUTC(2018, 5, 5, 10, 15)),
+        Lyssa(fromUTC(2018, 5, 5, 21, 15)),
+        Melandru(fromUTC(2018, 5, 5, 16, 15));
 
-        Tournament(LocalDateTime startTime) {
+        private ZonedDateTime startTime;
+
+        Tournament(ZonedDateTime startTime) {
             this.startTime = startTime;
         }
     }
 
-    // need to account for timezones because the base tournament times are different in every timezone
-    private static LocalDateTime fromPST(int year, int month, int day, int hour, int minute) {
-        return ZonedDateTime.of(year, month, day, hour, minute, 0, 0, ZoneId.of("PST", ZoneId.SHORT_IDS)).withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime();
+    // convert from UTC to local time zone
+    private static ZonedDateTime fromUTC(int year, int month, int day, int hour, int minute) {
+        return ZonedDateTime.of(year, month, day, hour, minute, 0, 0, ZoneId.of("Z"))
+                .withZoneSameInstant(ZoneId.systemDefault());
     }
 
     public void printSchedule() {
         // get current time
-        LocalDateTime now = LocalDateTime.now();
+        ZonedDateTime now = ZonedDateTime.now();
 
         // find the time for the upcoming tournaments by adding 23 hours to each until they're in the future
         for (Tournament t : Tournament.values()) {
@@ -44,13 +45,15 @@ public class ATCalendar {
         Arrays.sort(next, (Tournament t1, Tournament t2) -> t1.startTime.compareTo(t2.startTime));
 
         // print upcoming daily tournaments
-        System.out.println("Upcoming ATs:");
+        System.out.println("Upcoming ATs starting in:");
+
         for (Tournament t : next) {
-            System.out.println(t + ": " + until(now, t.startTime) + " - " + t.startTime.format(DateTimeFormatter.ofPattern("h:mm a EEEE")));
+            System.out.println(t + ": " + until(now, t.startTime) + " - "
+                    + t.startTime.format(DateTimeFormatter.ofPattern("h:mm a EEEE")));
         }
 
         // find and print the next monthly tournament
-        LocalDateTime monthly = fromPST(2018, 5, 26, 12, 15);
+        ZonedDateTime monthly = fromUTC(2018, 5, 26, 19, 15);
         while (monthly.isBefore(now)) {
             monthly = monthly.plusMonths(1);
         }
@@ -58,14 +61,15 @@ public class ATCalendar {
         System.out.println("Monthly: " + monthly.format(DateTimeFormatter.ofPattern("MMM d uuuu h:mm a EEEE")));
     }
 
-    private String until(LocalDateTime from, LocalDateTime to) {
+    // returns a String with the remaining time in hours and minutes
+    private String until(Temporal from, Temporal to) {
         long difference = ChronoUnit.MILLIS.between(from, to);
 
         //long diffSeconds = difference / 1000 % 60;
-		long diffMinutes = difference / (60 * 1000) % 60;
-		long diffHours = difference / (60 * 60 * 1000) % 24;
-		//long diffDays = difference / (24 * 60 * 60 * 1000);
-    
+        long diffMinutes = difference / (60 * 1000) % 60;
+        long diffHours = difference / (60 * 60 * 1000) % 24;
+        //long diffDays = difference / (24 * 60 * 60 * 1000);
+
         return String.format("%d hours %d minutes", diffHours, diffMinutes);
     }
 
